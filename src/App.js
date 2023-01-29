c74f2e5d85e5ab8be65b3a7384f7176c71a74738
Gaurav <gauravburande2425@gmail.com>
2023-01-29 23:28:07 +0530

    Table Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
+import Dashboard from "../src/pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Dashboard></Dashboard>
       {/* <ForgotPassword></ForgotPassword> */}
     </div>
   );
